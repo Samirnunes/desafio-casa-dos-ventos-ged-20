1690883af66c27aec3575f2810bba85c95eaee90
DeodatoBastos <deodatobasto@gmail.com>
2024-06-14 17:39:34 -0300

    feat: add stationary check and acf and pact plots

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -2,6 +2,10 @@ import os
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from tabulate import tabulate
 from PIL import Image
 
 
@@ -24,7 +28,7 @@ def plot_plant_ts_accumulated_by_month(ts_dict, plant="PSATCNV", close=True):
     ts = ts_dict[plant]
     df = ts.copy()
     df.index = pd.to_datetime(df.index)
-    df = df.resample('M').sum()
+    df = df.resample('ME').sum()
     df.plot(color="darkblue", legend=False)
     plt.title(f"Precipitação média acumulada por mês para {plant}")
     plt.xlabel("Data de referência")
@@ -44,7 +48,7 @@ def plot_plant_ts_accumulated_by_year(ts_dict, plant="PSATCNV", close=True):
     df = ts.copy()
     df.index = pd.to_datetime(df.index)
     df = df[df.index < "2024-01-01"]
-    df = df.resample('Y').sum()
+    df = df.resample('YE').sum()
     df.plot(color="darkblue", legend=False)
     plt.title(f"Precipitação média acumulada por ano para {plant}")
     plt.xlabel("Data de referência")
@@ -64,7 +68,7 @@ def plot_plant_ts_mean_by_month(ts_dict, plant="PSATCNV", close=True):
     ts = ts_dict[plant]
     df = ts.copy()
     df.index = pd.to_datetime(df.index)
-    df = df.resample('M').sum()
+    df = df.resample('ME').sum()
     df = df.groupby(df.index.month).mean()
     plt.bar(df.index, df["mean_precipitation"], color="darkblue")
     plt.xticks([i+1 for i in range(len(df.index))], list(df.index))
@@ -102,3 +106,55 @@ def precipitation_plots(ts_dict, plant="PSATCNV"):
     save_path = f"{folder_path}all.png"
     plt.savefig(save_path, bbox_inches='tight', pad_inches=0)
     plt.show()
+
+
+def is_stationary(ts_dict, plant="PSATCNV"):
+    ts = ts_dict[plant]
+    df = ts.copy()
+
+    result = adfuller(df["mean_precipitation"])
+
+    diff = df["mean_precipitation"].diff().dropna()
+    result_d = adfuller(diff)
+
+    filtered = df[df["mean_precipitation"] != 0]
+    log = np.log(filtered["mean_precipitation"])
+    log_diff = log.diff().dropna()
+    result_ld = adfuller(log_diff)
+
+    if (result[1] <= 0.05) & (result[4]['5%'] > result[0]):
+        stat = "\u001b[32mStationary\u001b[0m"
+    else:
+        stat = "\x1b[31mNon-stationary\x1b[0m"
+
+    if (result_d[1] <= 0.05) & (result_d[4]['5%'] > result_d[0]):
+        stat_d = "\u001b[32mStationary\u001b[0m"
+    else:
+        stat_d = "\x1b[31mNon-stationary\x1b[0m"
+
+    if (result_ld[1] <= 0.05) & (result_ld[4]['5%'] > result_ld[0]):
+        stat_ld = "\u001b[32mStationary\u001b[0m"
+    else:
+        stat_ld = "\x1b[31mNon-stationary\x1b[0m"
+
+    data = [
+        ["ADF Statistic", f"{result[0]:0.2f}", f"{result_d[0]:0.2f}", f"{result_ld[0]:0.2f}"],
+        ["p-value", f"{result[1]:0.2f}", f"{result_d[1]:0.2f}", f"{result_ld[1]:0.2f}"],
+        ["result", stat, stat_d, stat_ld]
+    ]
+    headers = ["", "Normal", "Diff", "LogDiff"]
+    print(tabulate(data, headers=headers, tablefmt="pretty"))
+
+
+def plot_acf_pacf(ts_dict, plant="PSATCNV"):
+    ts = ts_dict[plant]
+    df = ts.copy()
+    series = df["mean_precipitation"]
+
+    _, ax = plt.subplots(nrows=2, ncols=1, figsize=(12, 6))
+    plot_acf(series, lags=15, ax=ax[0])
+    plot_pacf(series, lags=15, ax=ax[1], method='ols')
+
+    plt.tight_layout()
+    plt.show()
+    plt.close()
