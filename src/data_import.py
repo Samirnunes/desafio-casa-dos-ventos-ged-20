56c3a00fb4cd09e055be55de3ada232f0d479750
DeodatoBastos <deodatobasto@gmail.com>
2024-06-15 13:40:36 -0300

    fix: fix new features

--- src/data_import.py
+++ src/data_import.py
@@ -28,35 +28,34 @@ def separate_predictions_plants_ts(name: str):
     df = pd.read_csv(f"../data/{name}.csv")
     df['date_ref'] = pd.to_datetime(df['date_ref'])
     df['date_forecast'] = pd.to_datetime(df['date_forecast'])
-    df['date_diff'] = (df['date_forecast'] - df['date_ref']).dt.days
+    df['date_diff'] = (df['date_ref'] - df['date_forecast']).dt.days
 
-    pivot_df = df.pivot_table(index=['ana_code', 'date_ref'],
+    pivot_df = df.pivot_table(index=['ana_code', 'date_forecast'],
                               columns='date_diff',
                               values='mean_precipitation',
                               fill_value=0)
     transformed_df = pivot_df.reset_index()
     transformed_df.columns.name = None  # Remove the column name (date_diff)
-    days_columns = [f'{name}_d+{col}' for col in transformed_df.columns[2:]]
-    transformed_df.columns = ['ana_code', 'date_ref'] + days_columns
+    days_columns = [f'{name}_d{col}' for col in transformed_df.columns[2:]]
+    transformed_df.columns = ['ana_code', 'date_forecast'] + days_columns
 
     for code in transformed_df["ana_code"].unique():
-        path = f"../data/ts-{code}.csv"
-        ts = pd.read_csv(path)
-        ts['date_ref'] = pd.to_datetime(ts['date_ref'])
+        path = f"../data/ts-{code}-{name}-model.csv"
         code_df = transformed_df[transformed_df["ana_code"] == code].drop(columns='ana_code')
-        merged = pd.merge(code_df, ts, on='date_ref', how='inner')
-        merged.to_csv(path, index=False)
+        code_df.to_csv(path, index=False)
 
 
 def create_new_features(ts_dict: dict):
     for key, df in ts_dict.items():
-
-        for i in range(1, 16):
-            df[f'-{i}d'] = df['mean_precipitation'].shift(i)
+        for i in range(1, 46):
+            df[f'lag_{i}'] = df['mean_precipitation'].shift(i)
 
         ts_dict[key] = df
-        df['mean_last_15d'] = df['mean_precipitation'].rolling(window=15).sum()
         df['mean_last_30d'] = df['mean_precipitation'].rolling(window=30).sum()
+        df['mean_last_60d'] = df['mean_precipitation'].rolling(window=60).sum()
+
+        path = f"../data/ts-{key}.csv"
+        df.to_csv(path)
 
 
 def split_train_test(ts_dict: dict):
