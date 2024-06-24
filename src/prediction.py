5ca7a0d7011adab4b0fb088e911936af873aebc7
Samir Nunes da Silva <samir.silva12342@gmail.com>
2024-06-24 15:28:46 -0300

    removed repeated functions

--- src/prediction.py
+++ src/prediction.py
@@ -86,33 +86,3 @@ def preprocess(X_train, X_test):
     X_train = pd.DataFrame(scaler.transform(X_train), columns=X_train.columns, index=X_train.index)
     X_test = pd.DataFrame(scaler.transform(X_test), columns=X_test.columns, index=X_test.index)
     return X_train, X_test
-
-
-def split_train_test_by_size(df: pd.DataFrame, test_size=50):
-    train_index = len(df) - test_size
-
-    X = df.drop(columns=['mean_precipitation'])
-    y = df['mean_precipitation']
-
-    X_train = X[:train_index]
-    X_test = X[train_index:]
-
-    y_train = y[:train_index]
-    y_test = y[train_index:]
-
-    return X_train, X_test, y_train, y_test
-
-
-def split_train_test_by_percentage(df: pd.DataFrame, percent=0.96):
-    train_index = math.ceil(len(df)*percent)
-
-    X = df.drop(columns=['mean_precipitation'])
-    y = df['mean_precipitation']
-
-    X_train = X[:train_index]
-    X_test = X[train_index:]
-
-    y_train = y[:train_index]
-    y_test = y[train_index:]
-
-    return X_train, X_test, y_train, y_test
